963894ceb72da5a9ca8a1e04f7f6aaa7ab01b6a3
Bachu90 <g.bach90@gmail.com>
2018-09-21 21:28:12 +0200

    21.09.18

--- js/script.js
+++ js/script.js
@@ -75,13 +75,20 @@ function topBarScroll() {
 }
 
 function windowScroll() {
+
     var elements = [
         document.querySelector(".about article:nth-child(1)"),
         document.querySelector(".about article:nth-child(2)"),
-        document.querySelector(".services")
+        document.querySelector(".services"),
+        document.querySelector(".why-best"),
+        document.querySelector(".testimonials"),
+        document.querySelector(".contact")
     ];
 
+    var counterSection = document.querySelector(".counter");
+
     function activate() {
+
         if (window.scrollY < 10) {
             elements.forEach(function (element) {
                 element.classList.add("hidden");
@@ -95,10 +102,90 @@ function windowScroll() {
         }
     }
 
+    function liveCounter() {
+        var counters = document.querySelectorAll(".counter div p:first-child");
+
+        function countTo(element) {
+            var startValue = parseInt(element.getAttribute("data-start"));
+            var endValue = parseInt(element.getAttribute("data-end"));
+            var value = startValue;
+
+            var count = setInterval(function () {
+                if ((value + 10000) < endValue) {
+                    value += 10000;
+                    element.textContent = value;
+                } else if ((value + 1000) < endValue) {
+                    value += 1000;
+                    element.textContent = value;
+                } else if ((value + 100) < endValue) {
+                    value += 10;
+                    element.textContent = value;
+                } else if (value < endValue) {
+                    value++;
+                    element.textContent = value;
+                } else {
+                    clearInterval(count);
+                }
+
+                if (value >= 1000000) {
+                    element.textContent = Math.round(parseFloat(value / 1000000) * 100) / 100 + "M";
+                }
+
+            }, 10);
+        }
+
+        if ((window.scrollY + window.innerHeight) > (counterSection.offsetTop + counterSection.offsetHeight)) {
+            if (counterSection.classList.contains("hidden")) {
+                counters.forEach(function (counter) {
+                    countTo(counter);
+                });
+                counterSection.classList.remove("hidden");
+            }
+        }
+
+        if (window.scrollY < 10) {
+            counters.forEach(function (counter) {
+                counter.textContent = counter.getAttribute("data-start");
+            });
+            counterSection.classList.add("hidden");
+        }
+
+
+    }
+
     window.addEventListener("scroll", activate);
+    window.addEventListener("scroll", liveCounter);
+}
+
+function goTopArrow() {
+    var header = document.querySelector("header");
+    var arrow = document.querySelector(".goTop");
+
+    arrow.classList.add("hidden");
+
+    function showArrow() {
+        if ((window.scrollY + window.innerHeight) > (2 * header.offsetHeight)) {
+            arrow.classList.remove("hidden");
+        } else {
+            arrow.classList.add("hidden");
+        }
+    }
+
+    arrow.addEventListener("click", function () {
+        var loop = setInterval(function () {
+            if (window.scrollY > 0) {
+                window.scrollTo(0, (window.scrollY - 20));
+            } else {
+                clearInterval(loop);
+            }
+        }, 1);
+    })
+
+    window.addEventListener("scroll", showArrow);
 }
 
 slide();
 menu();
 topBarScroll();
 windowScroll();
+goTopArrow();
